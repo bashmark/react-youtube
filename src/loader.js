510272725fbb883dd10a223f4d9d9ce5ca9834ef
Egor <bashmark@mail.ru>
2020-10-14 13:40:08 +0300

    first commit

---
+++ src/loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => (
+<div style={{display: 'flex', jusifyContent: 'center', margin: '5rem'}}>
+    <div className="lds-ellipsis">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</div>
+)
