88e1ed03d1d6839142b53c57862a8e098c10a076
Egor <bashmark@mail.ru>
2020-10-15 10:41:23 +0300

    Updates

---
+++ precache-manifest.527bd4baa6435ab7fcf1b6cac50f8bd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fde562b16f426197e7fb2f3350ffe4",
+    "url": "/react-youtube/index.html"
+  },
+  {
+    "revision": "b4f4a64904a6d5fc47da",
+    "url": "/react-youtube/static/css/main.10c6caa5.chunk.css"
+  },
+  {
+    "revision": "58c1e1c81fe5d9959ff1",
+    "url": "/react-youtube/static/js/2.facbde62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-youtube/static/js/2.facbde62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236911b7873e8b230fdc",
+    "url": "/react-youtube/static/js/3.455b243a.chunk.js"
+  },
+  {
+    "revision": "b4f4a64904a6d5fc47da",
+    "url": "/react-youtube/static/js/main.728c057e.chunk.js"
+  },
+  {
+    "revision": "f2ce59c8c266d010d042",
+    "url": "/react-youtube/static/js/runtime-main.74c95add.js"
+  }
+]);
